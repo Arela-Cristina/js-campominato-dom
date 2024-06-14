bd23c71b0de272faed7656f80693ebeb2d0e5f9f
Arela-Cristina <instagrandine@gmail.com>
2024-06-14 14:51:27 +0200

    Recycled the code from the js-campominato-grid repository

---
+++ funzioni.js
@@ -0,0 +1,8 @@
+// funzione per creare i cuadratini
+
+function gameStructure(tag, classToAdd) {
+    let box = document.createElement(tag);
+    box.innerHTML += i;
+    box.classList.add(classToAdd);
+    return box;
+}
