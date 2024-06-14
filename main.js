c64bad4513def6f5dc4a8129480213db8b0484d0
Arela-Cristina <instagrandine@gmail.com>
2024-06-14 20:49:26 +0200

    added campoSingolo.addEventListener('click', function ()  whit respectie conditions

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-let containerMinato = document.getElementById('campoMinato');
+const containerMinato = document.getElementById('campoMinato');
 // console.log(containerMinato);
 
-let myButton = document.getElementById('myButton');
+const myButton = document.getElementById('myButton');
 // console.log(myButton);
+
+//definisco una variabile per il punteggio
+const punteggio = document.getElementById('points');
+// console.log('sono il', punteggio.innerHTML);
+
 
 //creiamo un array di 16 numeri random
 let bomb = arrayNum(1, 100);
@@ -32,27 +37,32 @@ myButton.addEventListener('click', function () {
 
 
 
-        //Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro
+        //Aggiugiamo un evento. per farsi in modo che quando il utente clicka... 
         campoSingolo.addEventListener('click', function () {
-
-            if (campoSingolo[i]  === bomb) {
-
+            if (bomb.includes(i)) {
+                //condizione: la cella si colora di roso.
                 campoSingolo.classList.add('squareClickBomb');
-
+                //funzione per stoppare il gioco
+                endGame();
+           
             } else {
+                 //altrimenti: la cella si colora di verde.
                 campoSingolo.classList.add('squareClick');
+                //e quando si colora di verde conta i punti, che hanno un valore numerico
+                punteggio.innerHTML = parseInt(punteggio.innerHTML) + 1;
             }
             //ed emetto un messaggio in console con il numero  della cella cliccata.
-            // console.log('Cella clicata', this.innerHTML);
-
+            console.log('Cella clicata', this.innerHTML);
         });
     }
+    //Quando re inizio il gioco, il punteggio diventa 0
+    punteggio.innerHTML = parseInt(0);
+    
 });
 
 
-//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-//Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
-//In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
-//Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+
+
 //La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
 //Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
