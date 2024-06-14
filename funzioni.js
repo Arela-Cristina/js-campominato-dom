7bbec0b8ea6db3cf78bbe181a8882aef530ae0fd
Arela-Cristina <instagrandine@gmail.com>
2024-06-14 15:27:43 +0200

    Created a function -randomNumberGenerator
    -arrayRandomNum

--- funzioni.js
+++ funzioni.js
@@ -6,3 +6,17 @@ function gameStructure(tag, classToAdd) {
     box.classList.add(classToAdd);
     return box;
 }
+
+// funzione per creare un numero random
+function randomNumGenerator(numMin, numMax){
+    let numGen = Math.floor((Math.random() * (numMax - numMin +1)) + numMin);
+    return numGen
+}
+
+
+// funzione per creare un array con  numeri random da x fino a y
+function arrayRandomNum(min, max){
+
+    const arrayNum = [];
+    while(arrayNum.length <= max)
+}
